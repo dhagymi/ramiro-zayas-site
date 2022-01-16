64ca3826b118b0ab879cf23786191f1197c5e9ad
agusand <amandreacchi@gmail.com>
2022-01-16 19:02:36 -0300

    Concerts fields conditionals

--- app/utils/concerts.js
+++ app/utils/concerts.js
@@ -18,16 +18,37 @@ export const innerHtmlTemplate = ({
 	hours,
 	minutes,
 }) => {
+	console.log(price);
 	return `<article class="concerts__shows__cardWrapper" data-animation="fade">
 				<p class="concerts__shows__date">${day}.${month}.${year}</p>
 				<h2 class="concerts__shows__title">${title}</h2>
 				<div class="concerts__shows__content">
-					<p class="concerts__shows__info">Time: ${hours}:${minutes}</p>
-					<p class="concerts__shows__info">Location: ${location} <a href="https://google.com.ar/maps/search/${location.replace(
-		" ",
-		"+"
-	)}" target="_blank">(map)</a></p><p class="concerts__shows__info">Price: €${price}</p><p class="concerts__shows__info">Status: ${status}</p>
-					<p class="concerts__shows__info">Description: ${description}</p>
+					${
+						hours && minutes
+							? `<p class="concerts__shows__info">Time: ${hours}:${minutes}</p>`
+							: ""
+					}
+					${
+						location
+							? `<p class="concerts__shows__info">Location: ${location} <a href="https://google.com.ar/maps/search/${location.replace(
+									" ",
+									"+"
+							  )}" target="_blank">(map)</a></p>`
+							: ""
+					}
+					${
+						price && price != 0
+							? `<p class="concerts__shows__info">Price: €${price}</p>`
+							: price && price == 0
+							? `<p class="concerts__shows__info">Price: Free</p>`
+							: ""
+					}
+					${status ? `<p class="concerts__shows__info">Status: ${status}</p>` : ""}
+					${
+						description
+							? `<p class="concerts__shows__info">Description: ${description}</p>`
+							: ""
+					}
 				</div>
 			</article>`;
 };
