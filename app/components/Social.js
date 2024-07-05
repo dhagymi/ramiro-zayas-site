fe2aa92791a9e60353e41458ff2eb2cea5a2b2a1
agustinandreacchisw <agustin.andreacchi@southworks.com>
2024-07-05 13:13:35 +0100

    Adding prettier file and reformating changes

--- app/components/Social.js
+++ app/components/Social.js
@@ -5,113 +5,124 @@ import Component from "classes/Component.js";
 import HideHover from "classes/HideHover.js";
 
 export default class Options extends Component {
-  constructor() {
-    super({
-      element: ".social",
-      elements: {},
-      generalComponents: {
-        list: ".social__list",
-        listItems: ".social__list__item",
-        musicCardNext: ".music__videos__card--next",
-      },
-    });
+    constructor() {
+        super({
+            element: ".social",
+            elements: {},
+            generalComponents: {
+                list: ".social__list",
+                listItems: ".social__list__item",
+                musicCardNext: ".music__videos__card--next",
+            },
+        });
 
-    this.create();
-  }
+        this.create();
+    }
 
-  /* Create */
+    /* Create */
 
-  create() {
-    super.create();
+    create() {
+        super.create();
 
-    this.socialMediaQuantity = this.generalComponents?.listItems?.length / 2;
+        this.socialMediaQuantity =
+            this.generalComponents?.listItems?.length / 2;
 
-    if (this.generalComponents.musicCardNext) {
-      this.listItemHideHovers = map(
-        this.generalComponents.listItems,
-        (item, index) => {
-          if (index < this.socialMediaQuantity) {
-            return new HideHover({
-              element: `.social__list__item:nth-child(${index + 1})`,
-              generalComponents: {
-                toHover: this.generalSelectors.musicCardNext,
-              },
+        if (this.generalComponents.musicCardNext) {
+            this.listItemHideHovers = map(
+                this.generalComponents.listItems,
+                (item, index) => {
+                    if (index < this.socialMediaQuantity) {
+                        return new HideHover({
+                            element: `.social__list__item:nth-child(${
+                                index + 1
+                            })`,
+                            generalComponents: {
+                                toHover: this.generalSelectors.musicCardNext,
+                            },
+                        });
+                    }
+                }
+            );
+        }
+    }
+
+    update() {
+        if (this.generalComponents.musicCardNext) {
+            each(this.listItemHideHovers, (hideHover) => {
+                if (hideHover) {
+                    hideHover.update();
+                }
             });
-          }
         }
-      );
-    }
-  }
-
-  update() {
-    if (this.generalComponents.musicCardNext) {
-      each(this.listItemHideHovers, (hideHover) => {
-        if (hideHover) {
-          hideHover.update();
-        }
-      });
-    }
-  }
-
-  /* Animations */
-
-  showElement(elements) {
-    return new Promise((resolve) => {
-      this.timeline = GSAP.timeline();
-      if (Array.isArray(elements)) {
-        elements.forEach((element) => {
-          this.timeline.to(element, {
-            autoAlpha: 1,
-            onComplete: resolve,
-          });
-        });
-      } else {
-        this.timeline.to(elements, {
-          autoAlpha: 1,
-          onComplete: resolve,
-        });
-      }
-    });
-  }
-
-  hideElement(elements) {
-    return new Promise((resolve) => {
-      this.timeline = GSAP.timeline();
-      if (Array.isArray(elements)) {
-        elements.forEach((element) => {
-          this.timeline.to(element, { autoAlpha: 0, onComplete: resolve });
-        });
-      } else {
-        this.timeline.to(elements, { autoAlpha: 0, onComplete: resolve });
-      }
-    });
-  }
-
-  /* Events */
-  onChange() {
-    this.generalComponents.musicCardNext = document.querySelector(
-      this.generalSelectors.musicCardNext
-    );
-    if (this.generalComponents.musicCardNext) {
-      this.listItemHideHovers = map(
-        this.generalComponents.listItems,
-        (item, index) => {
-          if (index < this.socialMediaQuantity) {
-            return new HideHover({
-              element: `.social__list__item:nth-child(${index + 1})`,
-              generalComponents: {
-                toHover: this.generalSelectors.musicCardNext,
-              },
-            });
-          }
-        }
-      );
     }
 
-    each(this.generalComponents.listItems, (item, index) => {
-      if (index < this.socialMediaQuantity) {
-        this.showElement(item);
-      }
-    });
-  }
+    /* Animations */
+
+    showElement(elements) {
+        return new Promise((resolve) => {
+            this.timeline = GSAP.timeline();
+            if (Array.isArray(elements)) {
+                elements.forEach((element) => {
+                    this.timeline.to(element, {
+                        autoAlpha: 1,
+                        onComplete: resolve,
+                    });
+                });
+            } else {
+                this.timeline.to(elements, {
+                    autoAlpha: 1,
+                    onComplete: resolve,
+                });
+            }
+        });
+    }
+
+    hideElement(elements) {
+        return new Promise((resolve) => {
+            this.timeline = GSAP.timeline();
+            if (Array.isArray(elements)) {
+                elements.forEach((element) => {
+                    this.timeline.to(element, {
+                        autoAlpha: 0,
+                        onComplete: resolve,
+                    });
+                });
+            } else {
+                this.timeline.to(elements, {
+                    autoAlpha: 0,
+                    onComplete: resolve,
+                });
+            }
+        });
+    }
+
+    /* Events */
+    onChange() {
+        this.generalComponents.musicCardNext = document.querySelector(
+            this.generalSelectors.musicCardNext
+        );
+        if (this.generalComponents.musicCardNext) {
+            this.listItemHideHovers = map(
+                this.generalComponents.listItems,
+                (item, index) => {
+                    if (index < this.socialMediaQuantity) {
+                        return new HideHover({
+                            element: `.social__list__item:nth-child(${
+                                index + 1
+                            })`,
+                            generalComponents: {
+                                toHover: this.generalSelectors.musicCardNext,
+                            },
+                        });
+                    }
+                }
+            );
+        }
+
+        each(this.generalComponents.listItems, (item, index) => {
+            if (index < this.socialMediaQuantity) {
+                this.showElement(item);
+            }
+        });
+    }
 }
