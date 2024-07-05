b7c5b853412398bbfbe70e1b68c00f12735db15c
agustinandreacchisw <agustin.andreacchi@southworks.com>
2024-07-05 13:11:36 +0100

    Fixing social media icons behaviour

--- app/components/Social.js
+++ app/components/Social.js
@@ -5,111 +5,113 @@ import Component from "classes/Component.js";
 import HideHover from "classes/HideHover.js";
 
 export default class Options extends Component {
-	constructor() {
-		super({
-			element: ".social",
-			elements: {},
-			generalComponents: {
-				list: ".social__list",
-				listItems: ".social__list__item",
-				musicCardNext: ".music__videos__card--next",
-			},
-		});
+  constructor() {
+    super({
+      element: ".social",
+      elements: {},
+      generalComponents: {
+        list: ".social__list",
+        listItems: ".social__list__item",
+        musicCardNext: ".music__videos__card--next",
+      },
+    });
 
-		this.create();
-	}
+    this.create();
+  }
 
-	/* Create */
+  /* Create */
 
-	create() {
-		super.create();
+  create() {
+    super.create();
 
-		if (this.generalComponents.musicCardNext) {
-			this.listItemHideHovers = map(
-				this.generalComponents.listItems,
-				(item, index) => {
-					if (index <= 3) {
-						return new HideHover({
-							element: `.social__list__item:nth-child(${index + 1})`,
-							generalComponents: {
-								toHover: this.generalSelectors.musicCardNext,
-							},
-						});
-					}
-				}
-			);
-		}
-	}
+    this.socialMediaQuantity = this.generalComponents?.listItems?.length / 2;
 
-	update() {
-		if (this.generalComponents.musicCardNext) {
-			each(this.listItemHideHovers, (hideHover) => {
-				if (hideHover) {
-					hideHover.update();
-				}
-			});
-		}
-	}
+    if (this.generalComponents.musicCardNext) {
+      this.listItemHideHovers = map(
+        this.generalComponents.listItems,
+        (item, index) => {
+          if (index < this.socialMediaQuantity) {
+            return new HideHover({
+              element: `.social__list__item:nth-child(${index + 1})`,
+              generalComponents: {
+                toHover: this.generalSelectors.musicCardNext,
+              },
+            });
+          }
+        }
+      );
+    }
+  }
 
-	/* Animations */
+  update() {
+    if (this.generalComponents.musicCardNext) {
+      each(this.listItemHideHovers, (hideHover) => {
+        if (hideHover) {
+          hideHover.update();
+        }
+      });
+    }
+  }
 
-	showElement(elements) {
-		return new Promise((resolve) => {
-			this.timeline = GSAP.timeline();
-			if (Array.isArray(elements)) {
-				elements.forEach((element) => {
-					this.timeline.to(element, {
-						autoAlpha: 1,
-						onComplete: resolve,
-					});
-				});
-			} else {
-				this.timeline.to(elements, {
-					autoAlpha: 1,
-					onComplete: resolve,
-				});
-			}
-		});
-	}
+  /* Animations */
 
-	hideElement(elements) {
-		return new Promise((resolve) => {
-			this.timeline = GSAP.timeline();
-			if (Array.isArray(elements)) {
-				elements.forEach((element) => {
-					this.timeline.to(element, { autoAlpha: 0, onComplete: resolve });
-				});
-			} else {
-				this.timeline.to(elements, { autoAlpha: 0, onComplete: resolve });
-			}
-		});
-	}
+  showElement(elements) {
+    return new Promise((resolve) => {
+      this.timeline = GSAP.timeline();
+      if (Array.isArray(elements)) {
+        elements.forEach((element) => {
+          this.timeline.to(element, {
+            autoAlpha: 1,
+            onComplete: resolve,
+          });
+        });
+      } else {
+        this.timeline.to(elements, {
+          autoAlpha: 1,
+          onComplete: resolve,
+        });
+      }
+    });
+  }
 
-	/* Events */
-	onChange() {
-		this.generalComponents.musicCardNext = document.querySelector(
-			this.generalSelectors.musicCardNext
-		);
-		if (this.generalComponents.musicCardNext) {
-			this.listItemHideHovers = map(
-				this.generalComponents.listItems,
-				(item, index) => {
-					if (index <= 3) {
-						return new HideHover({
-							element: `.social__list__item:nth-child(${index + 1})`,
-							generalComponents: {
-								toHover: this.generalSelectors.musicCardNext,
-							},
-						});
-					}
-				}
-			);
-		}
+  hideElement(elements) {
+    return new Promise((resolve) => {
+      this.timeline = GSAP.timeline();
+      if (Array.isArray(elements)) {
+        elements.forEach((element) => {
+          this.timeline.to(element, { autoAlpha: 0, onComplete: resolve });
+        });
+      } else {
+        this.timeline.to(elements, { autoAlpha: 0, onComplete: resolve });
+      }
+    });
+  }
 
-		each(this.generalComponents.listItems, (item, index) => {
-			if (index <= 3) {
-				this.showElement(item);
-			}
-		});
-	}
+  /* Events */
+  onChange() {
+    this.generalComponents.musicCardNext = document.querySelector(
+      this.generalSelectors.musicCardNext
+    );
+    if (this.generalComponents.musicCardNext) {
+      this.listItemHideHovers = map(
+        this.generalComponents.listItems,
+        (item, index) => {
+          if (index < this.socialMediaQuantity) {
+            return new HideHover({
+              element: `.social__list__item:nth-child(${index + 1})`,
+              generalComponents: {
+                toHover: this.generalSelectors.musicCardNext,
+              },
+            });
+          }
+        }
+      );
+    }
+
+    each(this.generalComponents.listItems, (item, index) => {
+      if (index < this.socialMediaQuantity) {
+        this.showElement(item);
+      }
+    });
+  }
 }
