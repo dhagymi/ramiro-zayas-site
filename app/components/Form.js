3438aa0373fa63f17a91ad0ecb786ae4276c64bc
agustinandreacchisw <agustin.andreacchi@southworks.com>
2024-07-08 20:55:31 +0100

    Adding checkbox to form

--- app/components/Form.js
+++ app/components/Form.js
@@ -4,90 +4,134 @@ import GSAP from "gsap";
 import Component from "classes/Component.js";
 
 export default class Form extends Component {
-	constructor() {
-		super({
-			element: ".contact__form",
-			elements: {
-				successMessage: ".contact__form__message--success",
-				errorMessage: ".contact__form__message--error",
-			},
-		});
+        constructor() {
+                super({
+                        element: ".contact__form",
+                        elements: {
+                                successMessage:
+                                        ".contact__form__message--success",
+                                errorMessage: ".contact__form__message--error",
+                                checkedSvg: ".contact__form__inputs__checkbox__svgIcon--checked",
+                                checkbox: ".contact__form__inputs__checkbox__input",
+                        },
+                });
 
-		this.onSubmitEvent = this.onSubmit.bind(this);
+                this.onSubmitEvent = this.onSubmit.bind(this);
+                this.onCheckboxChangedEvent = this.onCheckboxChanged.bind(this);
 
-		this.create();
-	}
+                this.create();
+        }
 
-	getFormData(form) {
-		this.formData = new FormData(form);
+        create() {
+                super.create();
 
-		this.usefulFormData = {
-			name: this.formData.get("name"),
-			subject: this.formData.get("subject"),
-			email: this.formData.get("e-mail"),
-			message: this.formData.get("message"),
-		};
-	}
+                this.hideAndShowCheckbox(this.elements.checkbox.checked);
+        }
 
-	async onSubmit(event) {
-		try {
-			this.isError = false;
-			this.isOk = false;
-			this.isLoading = true;
+        getFormData(form) {
+                this.formData = new FormData(form);
 
-			event.preventDefault();
+                this.usefulFormData = {
+                        name: this.formData.get("name"),
+                        subject: this.formData.get("subject"),
+                        email: this.formData.get("e-mail"),
+                        message: this.formData.get("message"),
+                };
+        }
 
-			this.getFormData(event.target);
+        async onSubmit(event) {
+                try {
+                        this.isError = false;
+                        this.isOk = false;
+                        this.isLoading = true;
 
-			this.htmlTemplate = `
+                        event.preventDefault();
+
+                        this.getFormData(event.target);
+
+                        this.htmlTemplate = `
 			<p><b>Nombre:</b> ${this.usefulFormData.name}</p>
 			<p><b>E-mail:</b> ${this.usefulFormData.email}</p>
 			<p><b>Subject:</b> ${this.usefulFormData.subject}</p>
 			<p><b>Mensaje:</b> ${this.usefulFormData.message}</p>`;
 
-			this.response = await axios.post("/api/mail/contact", {
-				subject: `Mensaje de ${this.usefulFormData.name} - ${this.usefulFormData.subject}`,
-				html: this.htmlTemplate,
-			});
+                        this.response = await axios.post("/api/mail/contact", {
+                                subject: `Mensaje de ${this.usefulFormData.name} - ${this.usefulFormData.subject}`,
+                                html: this.htmlTemplate,
+                        });
 
-			if (this.response.status === 200) {
-				event.target.reset();
-				this.isOk = true;
-				this.isLoading = false;
-				this.showMessage(this.elements.successMessage);
-				setTimeout(() => {
-					this.isError = false;
-					this.isOk = false;
-				}, 3000);
-			}
-		} catch (error) {
-			this.isError = true;
-			this.isLoading = false;
-			this.showMessage(this.elements.errorMessage);
-			setTimeout(() => {
-				this.isError = false;
-				this.isOk = false;
-			}, 3000);
-			console.log(error);
-		}
-	}
+                        if (this.response.status === 200) {
+                                event.target.reset();
+                                this.isOk = true;
+                                this.isLoading = false;
+                                this.showMessage(this.elements.successMessage);
+                                setTimeout(() => {
+                                        this.isError = false;
+                                        this.isOk = false;
+                                }, 3000);
+                        }
+                } catch (error) {
+                        this.isError = true;
+                        this.isLoading = false;
+                        this.showMessage(this.elements.errorMessage);
+                        setTimeout(() => {
+                                this.isError = false;
+                                this.isOk = false;
+                        }, 3000);
+                        console.log(error);
+                }
+        }
 
-	showMessage(element) {
-		GSAP.to(element, { display: "block" });
-		GSAP.to(element, { autoAlpha: 1 });
-		setTimeout(() => {
-			GSAP.to(element, { autoAlpha: 0 });
-			GSAP.to(element, { display: "none" });
-		}, 3000);
-	}
+        showMessage(element) {
+                GSAP.to(element, { display: "block" });
+                GSAP.to(element, { autoAlpha: 1 });
+                setTimeout(() => {
+                        GSAP.to(element, { autoAlpha: 0 });
+                        GSAP.to(element, { display: "none" });
+                }, 3000);
+        }
 
-	/* Listeners */
+        onCheckboxChanged(event) {
+                this.hideAndShowCheckbox(event.target.checked);
+        }
 
-	addEventListeners() {
-		this.element.addEventListener("submit", this.onSubmitEvent);
-	}
+        hideAndShowCheckbox(isChecked) {
+                if (isChecked) {
+                        this.showCheckbox();
+                } else {
+                        this.hideCheckbox();
+                }
+        }
 
-	removeEventListeners() {
-		this.element.removeEventListener("submit", this.onSubmitEvent);
-	}
+        hideCheckbox() {
+                GSAP.to(this.elements.checkedSvg, {
+                        autoAlpha: 0,
+                        duration: 0.3,
+                });
+        }
+
+        showCheckbox() {
+                GSAP.to(this.elements.checkedSvg, {
+                        autoAlpha: 1,
+                        duration: 0.3,
+                });
+        }
+
+        /* Listeners */
+
+        addEventListeners() {
+                this.element.addEventListener("submit", this.onSubmitEvent);
+                this.elements.checkbox.addEventListener(
+                        "change",
+                        this.onCheckboxChangedEvent
+                );
+        }
+
+        removeEventListeners() {
+                this.element.removeEventListener("submit", this.onSubmitEvent);
+                this.elements.checkbox.removeEventListener(
+                        "change",
+                        this.onCheckboxChangedEvent
+                );
+        }
 }
