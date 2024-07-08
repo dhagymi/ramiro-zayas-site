e3c7f155e0137ebdb6d1fa0be75b4ce32c1b3618
agustinandreacchisw <agustin.andreacchi@southworks.com>
2024-07-08 17:51:00 +0100

    Adding imprint page

--- app.js
+++ app.js
@@ -221,6 +221,35 @@ app.get("/gallery", async (req, res) => {
         });
 });
 
+app.get("/imprint", async (req, res) => {
+        const api = await initApi(req);
+        const autoAdminApi = await initAutoAdminApi(req);
+
+        const defaults = await handleRequest({ api, autoAdminApi });
+        const imprint = await api.getSingle("imprint_page");
+
+        const personalInformation = imprint.data.personal_information
+                .split("\n")
+                .map((line) => line.trim());
+        const contactInformation = imprint.data.contact_information
+                .split("\n")
+                .map((line) => line.trim());
+
+        const modifiedImprint = {
+                ...imprint,
+                data: {
+                        ...imprint.data,
+                        personal_information: personalInformation,
+                        contact_information: contactInformation,
+                },
+        };
+
+        res.render("pages/imprint", {
+                ...defaults,
+                imprint: modifiedImprint,
+        });
+});
+
 app.get("/*", async (req, res) => {
         const api = await initApi(req);
         const autoAdminApi = await initAutoAdminApi(req);
