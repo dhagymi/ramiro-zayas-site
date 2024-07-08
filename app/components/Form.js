046cb1e1aebd8e6cf64722dc1dc56391e3ef505f
agustinandreacchisw <agustin.andreacchi@southworks.com>
2024-07-08 22:29:27 +0100

    Adding validation to form

--- app/components/Form.js
+++ app/components/Form.js
@@ -13,11 +13,14 @@ export default class Form extends Component {
                                 errorMessage: ".contact__form__message--error",
                                 checkedSvg: ".contact__form__inputs__checkbox__svgIcon--checked",
                                 checkbox: ".contact__form__inputs__checkbox__input",
+                                inputs: ".contact__form__inputs__input",
+                                submit_button: ".contact__form__inputs__button",
                         },
                 });
 
                 this.onSubmitEvent = this.onSubmit.bind(this);
                 this.onCheckboxChangedEvent = this.onCheckboxChanged.bind(this);
+                this.onChangeFormEvent = this.onChangeForm.bind(this);
 
                 this.create();
         }
@@ -26,6 +29,8 @@ export default class Form extends Component {
                 super.create();
 
                 this.hideAndShowCheckbox(this.elements.checkbox.checked);
+
+                this.onChangeForm();
         }
 
         getFormData(form) {
@@ -36,7 +41,55 @@ export default class Form extends Component {
                         subject: this.formData.get("subject"),
                         email: this.formData.get("e-mail"),
                         message: this.formData.get("message"),
+                        agree: !!this.formData.get("privacyPolicy"),
                 };
+        }
+
+        showMessage(element) {
+                GSAP.to(element, { display: "block" });
+                GSAP.to(element, { autoAlpha: 1 });
+                setTimeout(() => {
+                        GSAP.to(element, { autoAlpha: 0 });
+                        GSAP.to(element, { display: "none" });
+                }, 3000);
+        }
+
+        hideAndShowCheckbox(isChecked) {
+                if (isChecked) {
+                        this.showCheckbox();
+                } else {
+                        this.hideCheckbox();
+                }
+        }
+
+        hideCheckbox() {
+                GSAP.to(this.elements.checkedSvg, {
+                        autoAlpha: 0,
+                        duration: 0.3,
+                });
+        }
+
+        showCheckbox() {
+                GSAP.to(this.elements.checkedSvg, {
+                        autoAlpha: 1,
+                        duration: 0.3,
+                });
+        }
+
+        validate() {
+                if (!this.usefulFormData) return false;
+
+                if (Object.values(this.usefulFormData).some((value) => !value))
+                        return false;
+
+                if (
+                        !this.usefulFormData.email.match(
+                                new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)
+                        )
+                )
+                        return false;
+
+                return true;
         }
 
         async onSubmit(event) {
@@ -82,39 +135,14 @@ export default class Form extends Component {
                 }
         }
 
-        showMessage(element) {
-                GSAP.to(element, { display: "block" });
-                GSAP.to(element, { autoAlpha: 1 });
-                setTimeout(() => {
-                        GSAP.to(element, { autoAlpha: 0 });
-                        GSAP.to(element, { display: "none" });
-                }, 3000);
-        }
-
         onCheckboxChanged(event) {
                 this.hideAndShowCheckbox(event.target.checked);
         }
 
-        hideAndShowCheckbox(isChecked) {
-                if (isChecked) {
-                        this.showCheckbox();
-                } else {
-                        this.hideCheckbox();
-                }
-        }
+        onChangeForm() {
+                this.getFormData(this.element);
 
-        hideCheckbox() {
-                GSAP.to(this.elements.checkedSvg, {
-                        autoAlpha: 0,
-                        duration: 0.3,
-                });
-        }
-
-        showCheckbox() {
-                GSAP.to(this.elements.checkedSvg, {
-                        autoAlpha: 1,
-                        duration: 0.3,
-                });
+                this.elements.submit_button.disabled = !this.validate();
         }
 
         /* Listeners */
@@ -125,6 +153,13 @@ export default class Form extends Component {
                         "change",
                         this.onCheckboxChangedEvent
                 );
+                [this.elements.checkbox, ...this.elements.inputs].forEach(
+                        (input) =>
+                                input.addEventListener(
+                                        "change",
+                                        this.onChangeFormEvent
+                                )
+                );
         }
 
         removeEventListeners() {
@@ -132,6 +167,13 @@ export default class Form extends Component {
                 this.elements.checkbox.removeEventListener(
                         "change",
                         this.onCheckboxChangedEvent
+                );
+                [this.elements.checkbox, ...this.elements.inputs].forEach(
+                        (input) =>
+                                input.removeEventListener(
+                                        "change",
+                                        this.onChangeFormEvent
+                                )
                 );
         }
 }
